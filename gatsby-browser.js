c4952c3a8bbfc607be28e25a52fcaa55d3992dc4
Richard Bacon <richardjohnbacon@gmail.com>
2021-08-25 13:53:07 +0100

    Add css reset and base styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
 import '@fontsource/space-mono/700.css';
 import '@fontsource/lexend-deca/400.css';
+
 import './src/styles/global.css';
+import './src/styles/reset.css';
+import './src/styles/base.css';
