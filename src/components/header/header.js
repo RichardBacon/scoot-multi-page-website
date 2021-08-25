b3f3eb371f124fa8b20fb2ed0524a7bba21ce180
Richard Bacon <richardjohnbacon@gmail.com>
2021-08-25 16:07:42 +0100

    Add content and styling to header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,41 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
+import * as styles from './header.module.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
+        <div className={styles.headerSection}>
+          <Link className={styles.logo} to="/">
+            scoot
+          </Link>
+
+          <nav>
+            <ul className={styles.links}>
+              <li>
+                <Link className={styles.link} to="/about">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link} to="/locations">
+                  Locations
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link} to="/careers">
+                  Careers
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <button className={styles.button}>Get Scootin</button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
