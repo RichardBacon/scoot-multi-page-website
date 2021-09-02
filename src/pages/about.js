624109afbd6faedaae506d5f8812ddb942e498e8
Richard Bacon <richardjohnbacon@gmail.com>
2021-09-02 18:53:14 +0100

    Add features to about and careers pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 import Seo from '../components/seo/seo';
 import Layout from '../components/layout/layout';
 import PageBanner from '../components/pageBanner/pageBanner';
+import Feature from '../components/feature/feature';
+import * as styles from './about.module.css';
 
 const AboutPage = () => {
   return (
@@ -9,6 +12,32 @@ const AboutPage = () => {
       <Seo title="About" />
       <Layout>
         <PageBanner pageTitle="About" />
+        <section className={styles.featuresSection}>
+          <Feature
+            heading="Mobility for the digital era"
+            text="Getting around should be simple (and even fun!) for everyone. We embrace technology to provide low cost, smart access to scooters at your fingertips."
+            image={
+              <StaticImage
+                className={styles.image}
+                src="../assets/images/digital-era.jpg"
+                alt="A closeup of a lady looking at a mobile phone"
+              />
+            }
+          />
+
+          <Feature
+            heading="Better urban living"
+            text="We’re helping connect cities and bring people closer together. Our scooters are also fully-electric and we offset the minimal carbon footprint for each ride."
+            image={
+              <StaticImage
+                className={styles.image}
+                src="../assets/images/better-living.jpg"
+                alt="View looking down a city street with city buildings and a tram"
+              />
+            }
+            reverse={true}
+          />
+        </section>
       </Layout>
     </>
   );
--- src/pages/careers.js
+++ src/pages/careers.js
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 import Seo from '../components/seo/seo';
 import Layout from '../components/layout/layout';
 import PageBanner from '../components/pageBanner/pageBanner';
+import Feature from '../components/feature/feature';
+import * as styles from './careers.module.css';
 
 const CareersPage = () => {
   return (
@@ -9,6 +12,19 @@ const CareersPage = () => {
       <Seo title="Careers" />
       <Layout>
         <PageBanner pageTitle="Careers" />
+        <section className={styles.featuresSection}>
+          <Feature
+            heading="Care to join our mission?"
+            text="We’re always looking for ambitious individuals to help us on our journey. If you’re passionate about our mission to provide clean, accessible transport to improve urban living we want to hear from you!"
+            image={
+              <StaticImage
+                className={styles.image}
+                src="../assets/images/join-us.jpg"
+                alt="A group of people working on laptops in a modern office"
+              />
+            }
+          />
+        </section>
       </Layout>
     </>
   );
