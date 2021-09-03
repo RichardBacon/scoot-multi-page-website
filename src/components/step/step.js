0fe3ea497dd9968dcdfe34a491ae9a2413508211
Richard Bacon <richardjohnbacon@gmail.com>
2021-09-03 13:30:49 +0100

    Refactor steps section to use step component

---
+++ src/components/step/step.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as styles from './step.module.css';
+
+const Step = ({ icon, heading, text }) => {
+  return (
+    <div className={styles.step}>
+      {icon}
+
+      <div className={styles.content}>
+        <h4 className={styles.heading}>{heading}</h4>
+        <p className={styles.text}>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Step;
--- src/components/stepsSection/stepsSection.js
+++ src/components/stepsSection/stepsSection.js
@@ -1,56 +1,43 @@
 import * as React from 'react';
-import Line from '../../assets/patterns/line-grey.svg';
 import LocateIcon from '../../assets/icons/locate.svg';
 import ScooterIcon from '../../assets/icons/scooter.svg';
 import RideIcon from '../../assets/icons/ride.svg';
+import Step from '../step/step';
 import * as styles from './stepsSection.module.css';
 
 const StepsSection = () => {
+  const steps = [
+    {
+      id: 1,
+      icon: <LocateIcon />,
+      heading: 'Locate with app',
+      text: 'Use the app to find the nearest scooter to you. We are continuously placing scooters in the areas with most demand, so one should never be too far away.',
+    },
+    {
+      id: 2,
+      icon: <ScooterIcon />,
+      heading: 'Pick your scooter',
+      text: 'We show the most important info for the scooters closest to you. So you know how much charge they have left and can see roughly how much it will cost.',
+    },
+    {
+      id: 3,
+      icon: <RideIcon />,
+      heading: 'Enjoy the ride',
+      text: 'Scan the QR code and the bike will unlock. Retract the cable lock, put on a helmet, and you’re off! Always lock bikes away from walkways and accessibility ramps.',
+    },
+  ];
+
   return (
     <section className={styles.stepsSection}>
-      <div className={styles.lineWrapper}>
-        <Line className={styles.line} />
-      </div>
-
       <div className={styles.stepsWrapper}>
-        <div className={styles.stepCard}>
-          <LocateIcon />
-
-          <div className={styles.content}>
-            <h4 className={styles.heading}>Locate with app</h4>
-            <p className={styles.text}>
-              Use the app to find the nearest scooter to you. We are
-              continuously placing scooters in the areas with most demand, so
-              one should never be too far away.
-            </p>
-          </div>
-        </div>
-
-        <div className={styles.stepCard}>
-          <ScooterIcon />
-
-          <div className={styles.content}>
-            <h4 className={styles.heading}>Pick your scooter</h4>
-            <p className={styles.text}>
-              We show the most important info for the scooters closest to you.
-              So you know how much charge they have left and can see roughly how
-              much it will cost.
-            </p>
-          </div>
-        </div>
-
-        <div className={styles.stepCard}>
-          <RideIcon />
-
-          <div className={styles.content}>
-            <h4 className={styles.heading}>Enjoy the ride</h4>
-            <p className={styles.text}>
-              Scan the QR code and the bike will unlock. Retract the cable lock,
-              put on a helmet, and you’re off! Always lock bikes away from
-              walkways and accessibility ramps.
-            </p>
-          </div>
-        </div>
+        {steps.map((step) => (
+          <Step
+            key={step.id}
+            icon={step.icon}
+            heading={step.heading}
+            text={step.text}
+          />
+        ))}
       </div>
     </section>
   );
