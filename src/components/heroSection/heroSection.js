5a4fdad05c89189ffb2b4500b32fed0e51d5d632
Richard Bacon <richardjohnbacon@gmail.com>
2021-09-06 14:42:27 +0100

    Make hero section responsive on tablets

--- src/components/heroSection/heroSection.js
+++ src/components/heroSection/heroSection.js
@@ -8,25 +8,24 @@ import * as styles from './heroSection.module.css';
 const Hero = () => {
   return (
     <section className={styles.hero}>
-      <div className={styles.headingWrapper}>
+      <div className={styles.info}>
         <h1 className={styles.heading}>Scooter sharing made simple</h1>
+
+        <p className={styles.text}>
+          Scoot takes the hassle out of urban mobility. Our bikes are placed in
+          convenient locations in each of our cities. Use our app to locate the
+          nearest bike, unlock it with a tap, and you’re away!
+        </p>
+
+        <div className={styles.buttonWrapper}>
+          <Button>Get Scootin</Button>
+        </div>
       </div>
 
-      <div className={styles.info}>
-        <Line />
-        <div className={styles.text}>
-          <p>
-            Scoot takes the hassle out of urban mobility. Our bikes are placed
-            in convenient locations in each of our cities. Use our app to locate
-            the nearest bike, unlock it with a tap, and you’re away!
-          </p>
-        </div>
+      <div className={styles.icons}>
+        <Line className={styles.line} />
         <RightArrow className={styles.rightArrow} />
         <WhiteCircles className={styles.whiteCircles} />
-      </div>
-
-      <div className={styles.buttonWrapper}>
-        <Button>Get Scootin</Button>
       </div>
     </section>
   );
