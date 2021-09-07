e736ced6ebd4097221904f8c9de5e3ab5dbc5005
Richard Bacon <richardjohnbacon@gmail.com>
2021-09-07 12:36:06 +0100

    Prevent content from overflowing on mobiles devices

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,17 +2,18 @@ import * as React from 'react';
 import Header from '../header/header';
 import SignUp from '../signup/signup';
 import Footer from '../footer/footer';
+import * as styles from './layout.module.css';
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Header />
       <main>
         {children}
         <SignUp />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
